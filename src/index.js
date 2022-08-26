428135f170e44298cc54a572d8e3d3b788e5b9c4
Mark1237200 <0915kimjw@gmail.com>
2022-08-26 10:46:23 +0900

    test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import reactDOM from 'react-dom/client';
 import App from './App';
 
+const a = '1';
+console.log(a);
+
 const root = reactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
